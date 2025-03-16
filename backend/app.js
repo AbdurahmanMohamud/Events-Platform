597669392fc6dea844b142a9ee1d1b7d4908cb0b
Abdurahman <abdi_mo4@outlook.com>
2025-03-16 09:50:57 +0000

    minor changes

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,8 @@ const app = express();
 const PORT = process.env.PORT || 9090;
 
 // Middleware
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(helmet());
 app.use(morgan("dev"));
 
